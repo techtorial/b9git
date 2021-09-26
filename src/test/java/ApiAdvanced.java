c482bc80c45069f6a13a8ad14e49749132918c78
techtorial <info@techtorialacademy.com>
2021-09-26 13:38:35 -0500

    API advanced class

---
+++ src/test/java/ApiAdvanced.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class ApiAdvanced {
+
+    @Test
+    public void test() {
+
+
+        //api request
+        //new utils
+        //validation
+        //new api endpoints
+        System.out.println("Good job");
+
+
+    }
+
+
+}
